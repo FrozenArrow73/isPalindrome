1719b2cf00bf65b9432af3930d58732cd9a24723
Derek Wheeler <frozenarrow73@gmail.com>
2023-02-23 09:28:08 -0700

    finished psudocode

---
+++ isPalindrome.js
@@ -0,0 +1,13 @@
+// invoke the function and log the results
+
+//write the function
+//turn the word into an array of characters
+//create an empty forward array
+//create an empty backward array
+//loop through the word array
+//push each letter into the backwards array and unshift into the forwards array
+//end loop
+//turn forward array and backward array into strings
+//compare strings to see if they are equal
+// if they are return true
+// if they are not return false
